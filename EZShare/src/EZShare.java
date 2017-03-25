c54df137801ecd4f599261f2459b617471662d25
bkot <bkoteski88@gmail.com>
2017-03-25 23:25:09 +1100

    added json-simple-1.1.1.jar to EZShare project.

--- EZShare/src/EZShare.java
+++ EZShare/src/EZShare.java
@@ -1,11 +1,26 @@
-import EZShare.ArgsManager;
-import EZShare.ClientArgs;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class EZShare {
 
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 		// TODO
-		ArgsManager clientArgs = new ClientArgs(args);
+		JSONObject obj = new JSONObject();
+		obj.put("hello", "bob");
+		System.out.println(obj);
+		
+		JSONParser parser = new JSONParser();
+		String s = "{\"hello\":\"bob\"}";
+		try {
+			obj = (JSONObject) parser.parse(s);
+			System.out.println(obj);
+			System.out.println(obj.get("hello5"));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
